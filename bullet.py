2a3f37cd559bd05c416232c80b73f632f21a657b
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-01-15 01:10:14 -0300

    projetei_espaçonave

---
+++ bullet.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Uma classe que administra projeteis disparados pela espaçonave"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Cria um obejeto para o projetil na posiçao atual da espaçonave."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        # Cria um retangulo para o projetil em (0, 0) e, em seguida, define a
+        # posiçao correta
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+                                ai_settings.bullet_heigth)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Armazena a posiçao do projetil como um valor decimal
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings():
 
         # Configuraçao da espaçonave
         self.ship_speed_factor = 1.5
+
+        # Configuraçao do projeteis
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
