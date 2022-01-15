2435e494eb1ce22854cbcac4a5e006862cbcd2a2
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-01-14 23:57:50 -0300

    mudança_velocidade_ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,7 @@ def run_game():
     pygame.display.set_caption("Alien Ivansion")
 
     #Cria uma espaçonave
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # Define a cor de fundo
     bg_color = (230, 230, 230)
 
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Inicializa a espaçonave e define sua posiçao inicial"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Carrega a imagem da espaçonave e obtem seu rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,16 +16,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Armazena um valor decimal para o centro da espaçonave
+        self.center = float(self.rect.centerx)
+
         # Flag de movimento
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Atualiza a posiçao da espaçonave de acordo com a flag de movimento."""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -=1
+        # Atualiza o valor do centro da espaçonave, e nao o retangulo
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
+
+        # Atualiza o objeto rect de acordo com self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Desenha a espaçonave em sua posiçao atual"""
