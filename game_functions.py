5445e891da1b5b7211333a5240363b35b4f760f2
GuilhermeGarcia <guilhermegarci4@gmail.com>
2022-01-21 12:02:00 -0300

    aliens_se_movem

--- game_functions.py
+++ game_functions.py
@@ -110,6 +110,25 @@ def create_fleet(ai_settings, screen, ship, aliens):
                          row_number)
 
 
-def update_aliens(aliens):
-    """Atualiza as posiçoes de todos os alienigenas da frota."""
+def check_fleet_edges(ai_settings, aliens):
+    """Responde apropriadamente se algum alienigena alcançou uma borda."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_setthinds, aliens):
+    """Faz toda a frota descer e muda a sua direçao."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_setthinds.fleet_drop_speed
+    ai_setthinds.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    """
+    Verifica se a frota esta em uma das bordas
+    e entao atualiza as posiçoes de todos os alienigenas da frota.
+    """
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
